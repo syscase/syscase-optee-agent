0c3f5e16431cf057952e0f6ea4d7402ddf387b3c
Firas Zaidan <firas@zaidan.de>
2017-12-06 18:07:09 +0100

    Change behavior to open two sessions and contexts concurrently

--- host/main.c
+++ host/main.c
@@ -2,15 +2,20 @@
 
 int main(int argc, char *argv[])
 {
-  TEEC_Context ctx;
-  TEEC_Session sess;
+  TEEC_Context ctx, ctx2;
+  TEEC_Session sess, sess2;
   TEEC_UUID uuid = TA_AGENT_UUID;
 
   initializeContext(&ctx);
+  initializeContext(&ctx2);
   openSession(&ctx, &sess, &uuid);
+  openSession(&ctx2, &sess2, &uuid);
   invokeCall(&sess);
+  invokeCall(&sess2);
   closeSession(&sess);
+  closeSession(&sess2);
   finalizeContext(&ctx);
+  finalizeContext(&ctx2);
 
   return 0;
 }
