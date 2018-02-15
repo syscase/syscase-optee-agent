4dcab124ec27e8e2c8b35a95812da989f96f7e2a
Firas Zaidan <firas@zaidan.de>
2018-02-15 17:33:30 +0100

    Add support for variable system call argument array length

--- host/include/syscase/test_case.h
+++ host/include/syscase/test_case.h
@@ -6,6 +6,7 @@
 #define BUFFER_DELIMITER "\xa5\xc9"
 #define CALL_DELIMITER "\xb7\xe3"
 
+#define NARGS 8
 #define NBUFFERS 7
 #define SIZE_STACK_SIZE 256
 
@@ -13,7 +14,7 @@ extern int syscase_verbose;
 
 struct system_call {
     u_int16_t no;
-    u_int64_t args[6];
+    u_int64_t args[NARGS];
 };
 
 struct parse_state {
@@ -47,10 +48,10 @@ int parse_argument_vector_32(struct buffer *buffer, struct parse_state *state, u
 parse_handler_t parse_map(unsigned char type);
 
 int parse_argument(struct buffer *buffer, struct parse_state *state, u_int64_t *value);
-int parse_calls(struct system_call *calls, int ncalls, struct buffer *buffer, struct system_call *value);
-int parse_test_case(struct buffer *buffer, int max_calls, test_case_t *test_case, int *ncalls);
-void dump_call(struct system_call *value);
-void dump_test_case(test_case_t *value, int n);
+int parse_calls(struct system_call *calls, int max_args, int ncalls, struct buffer *buffer, struct system_call *value);
+int parse_test_case(struct buffer *buffer, int max_calls, int max_args, test_case_t *test_case, int *ncalls);
+void dump_call(struct system_call *value, int max_args);
+void dump_test_case(test_case_t *value, int n, int max_args);
 unsigned long execute_call(struct system_call *value);
 unsigned long execute_test_case(test_case_t *value, int n);
 
